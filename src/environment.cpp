061e78661539258f68bb69db20fd122f5ca6a150
José Ángel <joseangelmt@outlook.com>
2019-08-12 11:04:55 +0200

    Using the Lidar object

--- src/environment.cpp
+++ src/environment.cpp
@@ -46,6 +46,8 @@ void simpleHighway(pcl::visualization::PCLVisualizer::Ptr& viewer)
     std::vector<Car> cars = initHighway(renderScene, viewer);
     
 	auto lidar = new Lidar(cars, 0);
+	auto inputCloud = lidar->scan();
+	renderRays(viewer, lidar->position, inputCloud);
 
     // TODO:: Create point processor
   
