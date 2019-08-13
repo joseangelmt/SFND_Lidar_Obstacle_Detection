60bebb836bab62fbf2c4c16734d866f75cc0abaf
José Ángel <joseangelmt@outlook.com>
2019-08-13 09:55:48 +0200

    Searching Points in a KD-Tree

--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -45,13 +45,39 @@ struct KdTree
 	}
 
 	// return a list of point ids in the tree that are within distance of target
-	std::vector<int> search(std::vector<float> target, float distanceTol)
+	std::vector<int> search(const std::vector<float>& target, float distanceTol) const
 	{
 		std::vector<int> ids;
+		search(root, target, distanceTol, 0, ids);
 		return ids;
 	}
-	
 
+	void search(const Node* node, const std::vector<float>& target, float distanceTol, unsigned int depth, std::vector<int>& ids) const
+	{
+		if (node == nullptr)
+			return;
+
+		if (node->point[0] >= target[0] - distanceTol &&
+			node->point[0] <= target[0] + distanceTol &&
+			node->point[1] >= target[1] - distanceTol &&
+			node->point[1] <= target[1] + distanceTol) {
+
+			auto d{ 0.0 };
+			for (int i = 0; i < target.size(); i++)
+				d += pow(target[i] - node->point[i], 2.0);
+
+			if (sqrt(d) <= distanceTol)
+				ids.push_back(node->id);
+		}
+
+		const auto cd{ depth % 2 };
+
+		if (target[cd] - distanceTol < node->point[cd])
+			search(node->left, target, distanceTol, depth + 1, ids);
+
+		if (target[cd] + distanceTol > node->point[cd])
+			search(node->right, target, distanceTol, depth + 1, ids);
+	}
 };
 
 
